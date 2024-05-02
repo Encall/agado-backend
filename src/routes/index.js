792b8935089df5224bfb6d6cae536e0c1a13855c
Faan <rudfanno1@gmail.com>
2024-05-02 19:00:07 +0700

    add: initialize n number of airports

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const db = require('../configs/db');
 const authRoute = require('./auth.route');
 const dashboardRoute = require('./dashboard.route');
 const schema = require('../drizzle/schema');
-const { like, or } = require('drizzle-orm');
+const { like, or, eq, inArray } = require('drizzle-orm');
 
 router.use('/', authRoute);
 router.use('/dashboard', dashboardRoute);
@@ -53,6 +53,41 @@ router.get('/search/airports', async (req, res) => {
         console.error(error);
         res.status(500).json({
             message: 'An error occurred while searching airports',
+        });
+    }
+});
+
+// GET /search/airports/random 
+// * for recommend specify number of airports from client
+router.get('/search/airports/recommend', async (req, res) => {
+    try {
+        const number = req.query.number;
+        const airportIdObjects = await db
+            .select({ airportID: schema.airport.airportID })
+            .from(schema.airport);
+
+        const randomAirportIds = [];
+        for (let i = 0; i < number; i++) {
+            const randomIndex = Math.floor(
+                Math.random() * airportIdObjects.length
+            );
+            randomAirportIds.push(airportIdObjects[randomIndex].airportID);
+        }
+
+        const airports = await db
+            .select({
+                airportName: schema.airport.airportName,
+                city: schema.airport.city,
+                country: schema.airport.countryCode,
+                iata: schema.airport.IATACode,
+            })
+            .from(schema.airport)
+            .where(inArray(schema.airport.airportID, randomAirportIds));
+        res.json(airports);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'An error occurred while recommending airports',
         });
     }
 });
