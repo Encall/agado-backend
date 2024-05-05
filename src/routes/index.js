d7d2af35fba54e33c55513167a52fc45b360aa30
Encall <apichataimimpat@gmail.com>
2024-05-05 14:04:02 +0700

    implement health path

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const db = require('../configs/db');
 const authRoute = require('./auth.route');
 const dashboardRoute = require('./dashboard.route');
 const searchRoute = require('./search.route');
+const healthcheckRoute = require('./healthcheck.route');
 const schema = require('../drizzle/schema');
 
 router.use('/', authRoute);
+router.use('/health', healthcheckRoute);
 router.use('/dashboard', dashboardRoute);
 router.use('/search', searchRoute);
 
