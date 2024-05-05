825e056fa3a5afc4df86fb387948abab03de5ac4
Encall <apichataimimpat@gmail.com>
2024-05-05 18:08:26 +0700

    add read replica placeholder

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,3 +1,4 @@
+// const { withReplicas } = require('drizzle-orm/mysql-core');
 const { drizzle } = require('drizzle-orm/mysql2');
 const mysql = require('mysql2/promise');
 
@@ -8,6 +9,27 @@ const connection = mysql.createPool({
     database: process.env.DB_NAME,
     multipleStatements: true,
 });
+
+// const readReplica1client = mysql.createPool({
+//     host: process.env.DB_REPLICA1_HOST,
+//     user: process.env.DB_REPLICA1_USERNAME,
+//     password: process.env.DB_REPLICA1_PASSWORD,
+//     database: process.env.DB_REPLICA1_NAME,
+//     multipleStatements: true,
+// });
+
+// const readReplica2client = mysql.createPool({
+//     host: process.env.DB_REPLICA2_HOST,
+//     user: process.env.DB_REPLICA2_USERNAME,
+//     password: process.env.DB_REPLICA2_PASSWORD,
+//     database: process.env.DB_REPLICA2_NAME,
+//     multipleStatements: true,
+// });
+
+// const primary = drizzle(connection)
+// const replica1 = drizzle(readReplica1client)
+// const replica2 = drizzle(readReplica2client)
+// const db = withReplicas(drizzle(primary), replica1, replica2);
 
 const db = drizzle(connection, { mode: 'default' });
 
