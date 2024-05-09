6f518d2ac2cd8394af7e4d22631b2d9c6d305047
Encall <apichataimimpat@gmail.com>
2024-05-09 23:50:13 +0700

    Change database config to mysql

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,35 +2,16 @@
 const { drizzle } = require('drizzle-orm/mysql2');
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    multipleStatements: true,
+    multipleStatements: false,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
-// const readReplica1client = mysql.createPool({
-//     host: process.env.DB_REPLICA1_HOST,
-//     user: process.env.DB_REPLICA1_USERNAME,
-//     password: process.env.DB_REPLICA1_PASSWORD,
-//     database: process.env.DB_REPLICA1_NAME,
-//     multipleStatements: true,
-// });
-
-// const readReplica2client = mysql.createPool({
-//     host: process.env.DB_REPLICA2_HOST,
-//     user: process.env.DB_REPLICA2_USERNAME,
-//     password: process.env.DB_REPLICA2_PASSWORD,
-//     database: process.env.DB_REPLICA2_NAME,
-//     multipleStatements: true,
-// });
-
-// const primary = drizzle(connection)
-// const replica1 = drizzle(readReplica1client)
-// const replica2 = drizzle(readReplica2client)
-// const db = withReplicas(drizzle(primary), replica1, replica2);
-
-const db = drizzle(connection, { mode: 'default' });
 
 module.exports = db;
