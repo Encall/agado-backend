8972a7eeeb73282ee99409eb4d6d799853618679
chogerlate <siwaratlaopromger4444@gmail.com>
2024-05-25 09:33:53 +0700

    aircraft logic

--- src/drizzle/schema/aircraft.js
+++ src/drizzle/schema/aircraft.js
@@ -4,10 +4,11 @@ const airline = require('./airline');
 const aircraft = mysqlTable('aircraft', {
     aircraftID: int('aircraftID').primaryKey().autoincrement(),
     airlineID: int('airlineID').references(() => airline.airlineID, { onDelete: 'SET NULL' }, { onUpdate: 'CASCADE' }),
-    aircraftCallSign: varchar('aircraftCallSign', { length: 255 }).notNull(),
+    aircraftCallSign: varchar('aircraftCallSign', { length: 255 }).notNull().unique(),
     manufacturer: varchar('manufacturer', { length: 255 }).notNull(),
     model: varchar('model', { length: 255 }).notNull(),
     maxCapacity: int('maxCapacity').notNull(),
+    status: int('status').notNull(),
 });
 
 module.exports = aircraft;
